1291d236b7605b83bd0f70327a5edd4f87e9ecf7
alternateSS <potryasnyidjek@gmail.com>
2023-01-10 22:40:13 +0600

    First Commit

---
+++ posts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from django.contrib import admin
+
+from .models import StatusType, StatusCommentType
+admin.site.register(StatusType)
+admin.site.register(StatusCommentType)
